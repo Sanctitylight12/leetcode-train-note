739a27703b4e8db633bb232a286004ae909b99f7
Sanctitylight12 <sanctitylight12@gmail.com>
2023-07-19 15:02:33 +0800

    modified:   102. Binary Tree Level Order Traversal/102.cpp

--- 102. Binary Tree Level Order Traversal/102.cpp
+++ 102. Binary Tree Level Order Traversal/102.cpp
@@ -94,3 +94,41 @@ public:
 //         return ans;
 //     }
 // };
+
+
+// 100% faster
+
+
+// class Solution3 {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         if(root==NULL){
+//             vector<vector<int>> v;
+//             return v;
+//         }
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         q.push(NULL);
+//         vector<vector<int>>ans;
+//         vector<int>temp;
+//         while(!q.empty()){
+//             TreeNode* node=q.front();
+//             q.pop();
+//             if(node){
+//                 if(node->left) q.push(node->left);
+//                 if(node->right) q.push(node->right);
+                
+//                 temp.push_back(node->val);
+//             }else{
+//                 if(!q.empty()){
+//                     q.push(NULL);
+//                     ans.push_back(temp);
+//                     temp.resize(0);
+//                 }
+//             }
+            
+//         }
+//         ans.push_back(temp);
+//         return ans;
+//     }
+// };
