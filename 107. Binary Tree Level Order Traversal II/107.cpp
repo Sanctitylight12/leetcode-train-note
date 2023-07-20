2e89704f554f701a88b4595ec4e4f556cf306907
Sanctitylight12 <sanctitylight12@gmail.com>
2023-07-20 13:58:21 +0800

    new file:   107. Binary Tree Level Order Traversal II/107.cpp

---
+++ 107. Binary Tree Level Order Traversal II/107.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+
+  //Definition for a binary tree node.
+  struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+/*
+
+Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ans;     
+        if(root==NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+
+            int lens=q.size();
+            vector<int> level;
+
+            for(int i=0;i<lens;i++)
+            {
+               TreeNode* temp=q.front();
+               q.pop();
+               level.push_back(temp->val);
+               if(temp->left){q.push(temp->left);}
+               if(temp->right){q.push(temp->right);}
+            }
+            ans.push_back(level);
+
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+
+    }
+};
